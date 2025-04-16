fc288197de3bf7156cfde72e6eb450bdd1a85d97
augustorics <bboeissa@gmail.com>
2025-04-16 09:13:18 -0300

    Add files via upload

---
+++ repeticao2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int v = 1;
+    int n;
+    while(v >= 0)
+    {
+        printf("insira um numero: ");
+        scanf("%d",&v);
+        printf("%d \n", v);
+        if(v > n)
+        {
+            n = v;
+        }
+
+    }
+    printf(" Maior numero apresentado: %d", n);
+
+
+
+
+    return 0;
+}
+
